4d5f95ce8c8e6b87f72107b5b88d61df4adb5b51
chucuabac <ngochien96pt@gmail.com>
2019-06-14 01:32:46 +0700

    giao diện editor cơ bản(còn update)

--- models/drafpost.model.js
+++ models/drafpost.model.js
@@ -2,8 +2,7 @@ var db = require('../utils/database');
 
 module.exports = {
     byCat : (CatId) => {
-        return db.load(`select * from post where is_deleted=0 and id_category = ${CatId} order by date_posted desc`);
-        
+        return db.load(`select * from post where is_deleted=0 and (id_status=4 or id_status=3) and id_category = ${CatId} order by date_posted desc`);   
     },
 
 
